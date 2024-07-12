66519c554ba4f06b142e65b6c5f91686ce6778d4
Jon Nicholson <drjonnicholson@users.noreply.github.com>
2024-07-12 18:56:20 +0100

    just have header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import About from './components/About'
-import Acclaim from './components/Acclaim'
-import Experience from './components/Experience'
-import Footer from './components/Footer'
+// import About from './components/About'
+// import Acclaim from './components/Acclaim'
+// import Experience from './components/Experience'
+// import Footer from './components/Footer'
 import Header from './components/Header'
-import Publications from './components/Publications'
-import Skills from './components/Skills'
+// import Publications from './components/Publications'
+// import Skills from './components/Skills'
 
 const App = () => {
   return (
     <div>
       <Header />
-      <main>
+      {/* <main>
         <About />
         <Skills />
         <Experience />
         <Acclaim />
         <Publications />
       </main>
-      <Footer />
+      <Footer /> */}
     </div>
   )
 }
