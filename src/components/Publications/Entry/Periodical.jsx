0c70f2f7f779d803e8ceb93933745c1a25956fc7
Jon Nicholson <drjonnicholson@users.noreply.github.com>
2024-07-12 20:52:46 +0100

    add a few fields

--- src/components/Publications/Entry/Periodical.jsx
+++ src/components/Publications/Entry/Periodical.jsx
@@ -1,11 +1,11 @@
-// import { asMonth, quoteTitle } from '../../../utils'
+import { asMonth, quoteTitle } from '../../../utils'
 import Authors from './Authors'
 import PropTypes from 'prop-types'
 
 const Periodical = ({ entry }) => (
   <>
     <Authors>{entry.author}</Authors>,{' '}
-    {/* {quoteTitle(entry.title, [entry.journal, entry.volume, entry.number, entry.pages, entry.month, entry.year])}{' '} */}
+    {quoteTitle(entry.title, [entry.journal, entry.volume, entry.number, entry.pages, entry.month, entry.year])}{' '}
     {/* {entry.journal && (
       <>
         <i>{entry.journal}</i>,{' '}
@@ -13,9 +13,9 @@ const Periodical = ({ entry }) => (
     )}
     {entry.volume && <>vol. {entry.volume}, </>}
     {entry.number && <>no. {entry.number}, </>}
-    {entry.pages && <>pp. {entry.pages}, </>}
-    {entry.month && <>{asMonth(entry.month)} </>}
-    {entry.year && <>{entry.year}</>}
+    {entry.pages && <>pp. {entry.pages}, </>}*/}
+    {entry.month && asMonth(entry.month)}
+    {/*{entry.year && <>{entry.year}</>}
     {(entry.journal || entry.volume || entry.number || entry.pages || entry.month || entry.year) && <>.</>} */}
   </>
 )
