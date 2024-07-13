bcb629ca0e5e36ae33738c735d4a163ee31b3f1e
Jon Nicholson <drjonnicholson@users.noreply.github.com>
2024-07-13 17:15:58 +0100

    fixup the month conversion tool

--- src/components/Publications/Entry/Periodical.jsx
+++ src/components/Publications/Entry/Periodical.jsx
@@ -2,34 +2,23 @@ import { asMonth, quoteTitle } from '../../../utils'
 import Authors from './Authors'
 import PropTypes from 'prop-types'
 
-const Periodical = ({ entry }) => {
-  const title = quoteTitle(entry.title, [
-    entry.journal,
-    entry.volume,
-    entry.number,
-    entry.pages,
-    entry.month,
-    entry.year,
-  ])
-  const journal = entry.journal ? <i>{entry.journal}, </i> : ''
-  const volume = entry.volume ? `vol. ${entry.volume}, ` : ''
-  const number = entry.number ? `no. ${entry.number}, ` : ''
-  const pages = entry.pages ? `pp. ${entry.pages}, ` : ''
-  const month = entry.month ? '' : '' //`${asMonth(entry.month)} ` : ''
-  const year = entry.year ? entry.year : ''
-  const stop = entry.journal || entry.volume || entry.number || entry.pages || entry.month || entry.year ? '.' : ''
-  return (
-    <>
-      <Authors>{entry.author}</Authors>, {title} {journal}
-      {volume}
-      {number}
-      {pages}
-      {month}
-      {year}
-      {stop}
-    </>
-  )
-}
+const Periodical = ({ entry }) => (
+  <>
+    <Authors>{entry.author}</Authors>,{' '}
+    {quoteTitle(entry.title, [entry.journal, entry.volume, entry.number, entry.pages, entry.month, entry.year])}{' '}
+    {entry.journal && (
+      <>
+        <i>{entry.journal}</i>,{' '}
+      </>
+    )}
+    {entry.volume && <>vol. {entry.volume}, </>}
+    {entry.number && <>no. {entry.number}, </>}
+    {entry.pages && <>pp. {entry.pages}, </>}
+    {entry.month && <>{asMonth(entry.month)} </>}
+    {entry.year && <>{entry.year}</>}
+    {(entry.journal || entry.volume || entry.number || entry.pages || entry.month || entry.year) && <>.</>}
+  </>
+)
 
 Periodical.propTypes = {
   entry: PropTypes.shape({
--- src/utils/asMonth.js
+++ src/utils/asMonth.js
@@ -1,5 +1,18 @@
+const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+/**
+ * Little utility to parse the bibtex month to how we want to display it. It could have been a simpler map, but this
+ * lets us use `Intl.DateFormat` for more future control on how it should be displayed
+ *
+ * @param {string} bibMonth
+ * @returns {string}
+ */
 const asMonth = (bibMonth) => {
-  return Intl.DateTimeFormat('en', { month: 'long' }).format(new Date(`1/${bibMonth}/2000`))
+  const monthIdx = months.indexOf(bibMonth)
+  if (monthIdx < 0) {
+    throw new Error(`Month value '${bibMonth}' could not be converted to a month`)
+  }
+  return Intl.DateTimeFormat('en', { month: 'long' }).format(new Date(2000, monthIdx, 1))
 }
 
 export default asMonth
